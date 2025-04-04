c5dd5585efb2dcba576cefec98e7e3c3c00ee6f2
Scott Lusk <scottluskcis@github.com>
2025-04-04 18:07:27 -0400

    wire up to use harness

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,19 @@
-import { config } from 'dotenv';
-config();
+import {
+  executeWithOctokit,
+  getOptsFromEnv,
+} from '@scottluskcis/octokit-harness';
 
 console.log('Hello, world!');
+
+const opts = getOptsFromEnv();
+
+executeWithOctokit(opts, async ({ octokit, logger }) => {
+  logger.info('You got a logger!');
+
+  if (octokit) logger.info('You got an octokit client!');
+
+  //   const { data: user } = await octokit.rest.users.getByUsername({
+  //     username: 'scottlusk',
+  //   });
+  //   console.log(user);
+});
