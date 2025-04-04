f698b7ca29de57b64aff20f28b12bf40612863e3
Scott Lusk <scottluskcis@github.com>
2025-04-04 18:24:09 -0400

    add logic to get releases size

--- src/index.ts
+++ src/index.ts
@@ -3,17 +3,53 @@ import {
   getOptsFromEnv,
 } from '@scottluskcis/octokit-harness';
 
-console.log('Hello, world!');
+import { filesize } from 'filesize';
 
 const opts = getOptsFromEnv();
+const RELEASE_ASSETS_WARNING_THRESHOLD_IN_BYTES = 5 * 1000 * 1000 * 1000; // 5 GB
 
 executeWithOctokit(opts, async ({ octokit, logger }) => {
-  logger.info('You got a logger!');
+  logger.info('Starting...');
 
-  if (octokit) logger.info('You got an octokit client!');
+  const repoName = 'developer-portal';
 
-  //   const { data: user } = await octokit.rest.users.getByUsername({
-  //     username: 'scottlusk',
-  //   });
-  //   console.log(user);
+  const releasesIterator = octokit.paginate.iterator(
+    'GET /repos/{owner}/{repo}/releases',
+    {
+      owner: opts.orgName,
+      repo: repoName,
+      per_page: 100,
+    },
+  );
+
+  let releaseAssetsTotalSizeInBytes = 0;
+
+  for await (const { data: releases } of releasesIterator) {
+    const releaseAssets = releases.flatMap((release) => release.assets);
+
+    const releaseAssetsPageTotalInBytes = releaseAssets.reduce(
+      (total, asset) => total + asset.size,
+      0,
+    );
+
+    releaseAssetsTotalSizeInBytes += releaseAssetsPageTotalInBytes;
+
+    console.debug(
+      `Repo: ${repoName} Release assets found ${filesize(releaseAssetsPageTotalInBytes)}.`,
+    );
+  }
+
+  if (
+    releaseAssetsTotalSizeInBytes >= RELEASE_ASSETS_WARNING_THRESHOLD_IN_BYTES
+  ) {
+    logger.warn(
+      `Repo: ${repoName} Total size of release assets is ${filesize(releaseAssetsTotalSizeInBytes)}, which is above the warning threshold of ${filesize(RELEASE_ASSETS_WARNING_THRESHOLD_IN_BYTES)}.`,
+    );
+  } else {
+    logger.info(
+      `Repo: ${repoName} Total size of release assets is ${filesize(releaseAssetsTotalSizeInBytes)}.`,
+    );
+  }
+
+  logger.info('Done!');
 });
